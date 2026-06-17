97c7ddcb4527245b0e6632216127644ec57c9585
Nomso <nomsounaogu@gmail.com>
2026-06-17 11:32:36 +0100

    finished live campaigns UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-import { FeaturedCauses } from "@/components/home/featured-causes";
 import Hero from "@/components/home/hero";
-import FAQ from "@/components/home/frequentlyAskedQuestions";
-import AdBanner from "@/components/AdBanner";
-import LaunchYourCauseInSeconds from "@/components/home/launchYourCauseInSeconds";
-import { TrendingCauses } from "@/components/home/trendingNow";
-import WhyItStandsOut from "@/components/home/whyitStandsOut";
-import HowitWorksYT from "@/components/home/howitWorksYT";
-import { FeaturedPetitions } from "@/components/home/featured-petitions";
-import { UrgentCauses } from "@/components/home/urgentCauses";
-import CTASection from "@/components/home/cta-section";
-import Numbers from "@/components/numbers";
+
+import LiveCampaigns from "@/components/home/LiveCampaigns";
+
 import { AnnouncementMarquee } from "@/components/ui/announcement-marquee";
-import ProofFeatureSplit from "@/components/home/proof-feature-split";
-import WhyOnChain from "@/components/home/WhyOnChain";
+
 import { RoutedOnChain } from "@/components/home/RoutedOnChain";
 import { MoneyTrail } from "@/components/home/MoneyTrail";
 import { Metadata } from "next";
@@ -42,8 +33,7 @@ export default async function Home({
     <div className="flex flex-col min-h-screen mt-12 md:mt-16 ">
       <Hero />
 
-      <div className="mx-8">
-      </div>
+      <div className="mx-8"></div>
 
       <div className="mx-8">
         <RoutedOnChain />
@@ -55,40 +45,10 @@ export default async function Home({
 
       <AnnouncementMarquee />
 
+      <LiveCampaigns />
+
       <div className="mx-8">
         <Accountability />
-      </div>
-
-      {/* <ProofFeatureSplit /> */}
-
-      {/* <WhyOnChain /> */}
-
-      <div className="mx-8">
-        {/* <UrgentCauses /> */}
-      </div>
-
-      <div className="">
-        {/* <WhyItStandsOut /> */}
-      </div>
-
-      <div className="mx-8">
-        {/* <TrendingCauses /> */}
-      </div>
-
-      <div className="mx-8">
-        {/* <FeaturedPetitions /> */}
-      </div>
-
-      
-
-      <div className="">
-        {/* <HowitWorksYT /> */}
-      </div>
-
-      {/* <CTASection /> */}
-
-      <div id="faq" className="md:mx-8">
-        {/* <FAQ /> */}
       </div>
     </div>
   );
