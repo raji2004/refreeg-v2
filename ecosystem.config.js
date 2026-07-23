c5152f9492f83c60d7a0472463029e2050a03612
Gbiyede Victor <tega2112@gmail.com>
2026-07-23 15:21:24 +0100

    update to pm2 route

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       // ── Landing page ──────────────────────────────────────────────────────
       name: "frontend",
-      script: "node_modules/.bin/next",
+      script: "node_modules/next/dist/bin/next",
       args: "start",
       cwd: "/mnt/data/refreeg/current",
       env: {
@@ -44,7 +44,7 @@ module.exports = {
     {
       // ── API / App ─────────────────────────────────────────────────────────
       name: "api",
-      script: "node_modules/.bin/next",
+      script: "node_modules/next/dist/bin/next",
       args: "start",
       cwd: "/mnt/data/refreeg/current",
       env: {
